cc47175aea0b928a72381c6e2612e00fb68da8a1
VladaGaz <84187920+VladaGaz@users.noreply.github.com>
2021-06-11 00:01:35 +0300

    Delete store

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './routes'
-import store from './store'
-
 
 import './assets/scss/main.scss'
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app')
